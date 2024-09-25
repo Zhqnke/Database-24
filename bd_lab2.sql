ad677a72166e7601a3a26dfb317a7a06c03507da
Zhanerke <156804450+Zhqnke@users.noreply.github.com>
2024-09-25 14:58:46 +0600

    Update bd_lab2.sql

--- bd_lab2.sql
+++ bd_lab2.sql
@@ -23,5 +23,24 @@ VALUES ('China', 3, 1390000000),
 
 ALTER TABLE countries ALTER COLUMN country_name SET DEFAULT 'Kazakhstan';
 
-INSERT INTO countries (region_id, population)
-VALUES (5, 34000000);
+INSERT INTO countries (country_name, region_id, population)
+VALUES (DEFAULT, DEFAULT, DEFAULT);  -- Uses default values
+CREATE TABLE countries_new LIKE countries;
+
+INSERT INTO countries_new SELECT * FROM countries;
+
+UPDATE countries
+SET region_id = 1
+WHERE region_id IS NULL;
+
+SELECT country_name, 
+       population * 1.10 AS "New Population"
+FROM countries;
+
+DELETE FROM countries
+WHERE population < 100000;
+
+DELETE FROM countries_new
+WHERE country_id IN (SELECT country_id FROM countries);
+
+DELETE FROM countries;
